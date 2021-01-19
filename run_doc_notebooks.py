0bc4a73a0d89c9e6cdbb37942a85f31c22abe034
bergi <s.berke@netzkolchose.de>
2021-01-19 11:27:28 +0100

    refactor run_doc_notebooks a bit + add filtered markdown output

--- run_doc_notebooks.py
+++ run_doc_notebooks.py
@@ -16,15 +16,18 @@ DOCS_DIR = "docs"
 HIDDEN_CELLS = [
     r"^# hidden.*",
 ]
+RUN_BUT_HIDDEN_CELLS = [
+    "# run-but-hidden"
+]
 
 
-def export_notebook(filename):
+def export_notebook(filename, format, directory, do_rename_lexer=True):
     env = os.environ.copy()
     env["PYTHONPATH"] = ".."
     result = subprocess.call(
         [
             "jupyter", "nbconvert",
-            "--execute", "--to=rst", f"--output-dir={DOCS_DIR}",
+            "--execute", f"--to={format}", f"--output-dir={directory}",
             f"--RegexRemovePreprocessor.patterns={repr(HIDDEN_CELLS)}",
             filename,
         ],
@@ -34,6 +37,21 @@ def export_notebook(filename):
         raise AssertionError(
             f"Exporting notebook {filename} failed with exit-code {result}"
         )
+
+    ext = {"markdown": "md"}.get(format, format)
+    out_filename = filename.split(os.path.sep)[-1].replace(".ipynb", f".{ext}")
+
+    if do_rename_lexer:
+        # rename the "ipython3" lexer so readthedocs does not need ipython installed
+        #   It's safe because we do not use cell magic or other extended stuff
+        rename_lexer(
+            os.path.join(directory, out_filename),
+            "ipython3",
+            "python3"
+        )
+
+    if format == "markdown":
+        remove_hidden_cells_in_markdown(out_filename)
 
 
 def rename_lexer(filename, old, new):
@@ -49,6 +67,53 @@ def rename_lexer(filename, old, new):
             fp.write(replaced_text)
 
 
+def remove_hidden_cells_in_markdown(filename: str):
+    """
+    Loads a markdown file and removes all ``` blocks that
+    have the line "# run-but-hidden" in them.
+
+    Unfortunately the jupyter nbconvert RegexRemovePreprocessor removes
+    all hidden cells before execution, so if we need to run them but hide
+    the output this crappy patching is required.
+    """
+    with open(filename) as fp:
+        lines = fp.read().splitlines()
+
+    out_lines = []
+    block_lines = []
+    inside_block = False
+    block_hidden = False
+    for line in lines:
+        if line.startswith("```"):
+            if not inside_block:
+                inside_block = True
+            else:
+                if not block_hidden:
+                    out_lines += block_lines
+                    out_lines.append(line)
+                block_lines.clear()
+                inside_block = False
+                block_hidden = False
+                continue
+
+        if inside_block:
+            block_lines.append(line)
+
+            for tag in RUN_BUT_HIDDEN_CELLS:
+                if line.startswith(tag):
+                    block_hidden = True
+                    break
+
+        else:
+            out_lines.append(line)
+
+    if lines != out_lines:
+        print(f"removing hidden cells from {filename}")
+        text = "\n".join(out_lines)
+        with open(filename, "w") as fp:
+            fp.write(text)
+
+
 def render_tutorial():
     # delete the shapes index if it is present
     try:
@@ -62,9 +127,14 @@ def render_tutorial():
     except FileNotFoundError:
         pass
 
-    export_notebook("examples/tutorial.ipynb")
-    rename_lexer("docs/tutorial.rst", "ipython3", "python3")
+    export_notebook("examples/tutorial.ipynb", "rst", DOCS_DIR)
+
+
+def render_quickref():
+    export_notebook("docs/quickref.ipynb", "markdown", ".")
 
 
 if __name__ == "__main__":
-    render_tutorial()
+    #render_tutorial()
+    render_quickref()
+    #remove_hidden_cells_in_markdown("quickref.md")
