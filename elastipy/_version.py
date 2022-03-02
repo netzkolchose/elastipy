127a462f37fc2a2a58f506ae0c75b7c124f270a2
bergi <s.berke@netzkolchose.de>
2022-03-02 23:54:17 +0100

    add Search.highlight() and Search.version
    - highlught supports all parameters from current 8.0.0 docs and
    global as well as field levels.
    
    - Search instance now supports both version 7 and 8 via an init parameter.
      Defaults to elasticsearch-py major version
    
    - more version specific details and unittests

--- elastipy/_version.py
+++ elastipy/_version.py
@@ -1,12 +1,3 @@
-from elasticsearch import VERSION as ES_VERSION
-
 version = (0, 2, 1)
 
 version_str = "%s.%s.%s" % version
-
-
-if ES_VERSION[0] not in (7, 8):
-    raise ValueError(
-        f"Unsupported elasticsearch-py major version {ES_VERSION}"
-        f", expecting 7 or 8"
-    )
